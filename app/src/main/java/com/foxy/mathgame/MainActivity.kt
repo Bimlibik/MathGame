6af64fa1d1a56dbd6e9fe513018918ad8f219f1e
Factis <53155715qwerty>
2020-02-17 16:18:27 +0800

    init

---
+++ app/src/main/java/com/foxy/mathgame/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.foxy.mathgame
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_play.setOnClickListener { startGame() }
+        tv_play.setOnClickListener { startGame() }
+    }
+
+    private fun startGame() {
+        val intent = Intent(this, StartGame::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
---
+++ app/src/main/java/com/foxy/mathgame/StartGame.kt
@@ -0,0 +1,12 @@
+package com.foxy.mathgame
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StartGame : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
