8a24a247e286d492364388e5661598c7cb0c317a
Factis <53155715qwerty>
2020-02-18 15:33:30 +0800

    add personal best result

--- app/src/main/java/com/foxy/mathgame/GameOver.kt
+++ app/src/main/java/com/foxy/mathgame/GameOver.kt
@@ -1,19 +1,36 @@
 package com.foxy.mathgame
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_game_over.*
 
 class GameOver : AppCompatActivity() {
+
+    lateinit var pref: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_over)
         supportActionBar?.hide()
 
+        pref = getSharedPreferences("pref", 0)
+        var savedPoints = pref.getInt("savedPoints", 0)
+        val editor = pref.edit()
+
         val points = intent.getIntExtra("points", 0)
+
+        if (points > savedPoints) {
+            savedPoints = points
+            editor.putInt("savedPoints", savedPoints)
+            editor.apply()
+            tv_high_score.visibility = View.VISIBLE
+        }
+
         tv_points.text = points.toString()
+        tv_best.text = savedPoints.toString()
 
         btn_restart.setOnClickListener { restartGame() }
         btn_exit.setOnClickListener { exitGame() }
--- app/src/main/java/com/foxy/mathgame/StartGame.kt
+++ app/src/main/java/com/foxy/mathgame/StartGame.kt
@@ -3,6 +3,7 @@ package com.foxy.mathgame
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
